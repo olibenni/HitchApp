93c8ddadd29e28687d217caef536f067115e1248
Olafur Magnusson <olafurma@novomaticls.com>
2016-04-04 16:53:16 +0000

    Listviews items bigger, optimized imports

--- app/src/main/java/com/myapp/hitch3/LogIn.java
+++ app/src/main/java/com/myapp/hitch3/LogIn.java
@@ -3,23 +3,19 @@ package com.myapp.hitch3;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.StrictMode;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
 
 import com.facebook.CallbackManager;
 import com.facebook.FacebookCallback;
 import com.facebook.FacebookException;
+import com.facebook.FacebookSdk;
 import com.facebook.appevents.AppEventsLogger;
 import com.facebook.login.LoginManager;
 import com.facebook.login.LoginResult;
 import com.myapp.hitch3.util.API;
+
 import java.io.IOException;
-import com.facebook.FacebookSdk;
 
 /**
  * Created by olafurma 24.1.2016
--- app/src/main/java/com/myapp/hitch3/Passenger.java
+++ app/src/main/java/com/myapp/hitch3/Passenger.java
@@ -3,15 +3,14 @@ package com.myapp.hitch3;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
+
 import com.myapp.hitch3.util.API;
 
 import java.io.IOException;
